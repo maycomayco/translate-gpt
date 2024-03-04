00538fba4fece044d7df791c9ca2c09c3bbcc262
Mayco Barale <maycobarale@gmail.com>
2024-03-04 17:40:53 -0300

    Add @vercel/analytics dependency and import Analytics component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,15 @@
+import { Analytics } from '@vercel/analytics'
+
 export default function Layout({ children }) {
   return (
-    <main className="px-2">
-      <div className="container mx-auto grid min-h-screen max-w-prose grid-rows-[60px_1fr_60px] gap-8">
-        {children}
-      </div>
-    </main>
+    <>
+      <Analytics />
+
+      <main className="px-2">
+        <div className="container mx-auto grid min-h-screen max-w-prose grid-rows-[60px_1fr_60px] gap-8">
+          {children}
+        </div>
+      </main>
+    </>
   )
 }
